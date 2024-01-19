643b66fc59bddb6e4308c6e3d6084da2024f4029
HaroldCalvo <haroldcalvooo@gmail.com>
2024-01-19 13:42:43 +0800

    update scroll down smooth

--- js/function.js
+++ js/function.js
@@ -1,3 +1,16 @@
 function Artworks(){
     document.querySelector('#Skills').classList.toggle('ChangeArtworks');
 }
+$('.scroll-link').on('click', function(e) {
+    e.preventDefault(); // Prevent the default behavior of the anchor link
+
+    var target = $(this).data('target'); // Get the target section from the data-target attribute
+    var $targetSection = $('#' + target);
+
+    if ($targetSection.length) {
+        // Scroll to the section smoothly using scrollIntoView
+        $targetSection[0].scrollIntoView({
+            behavior: 'smooth'
+        });
+    }
+});
