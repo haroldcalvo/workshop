fc55947a6eedda5be7c95ae82478f84b842bfc12
haroldcalvo <147241098+haroldcalvo@users.noreply.github.com>
2024-05-01 18:42:10 +0800

    update

--- js/myFunction.js
+++ js/myFunction.js
@@ -1,8 +1,10 @@
 window.addEventListener('scroll', function() {
-  var element = document.querySelector('.description');
-  var position = element.getBoundingClientRect().top;
-  var windowHeight = window.innerHeight;
-  if (position < windowHeight * 0.75) {
+  var elements = document.querySelectorAll('.description');
+  elements.forEach(function(element) {
+    var position = element.getBoundingClientRect().top;
+    var windowHeight = window.innerHeight;
+    if (position < windowHeight * 0.75) {
       element.classList.add('show');
-  }
+    }
+  });
 });
